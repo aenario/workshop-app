60d36e33b1705df48ecfd689e9066b067f59dfee
Romain <romain@cozycloud.cc>
2016-05-19 13:28:39 +0200

    step1 : add cozysdk & fetch contacts

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('javascript works too ! ');
+// data management
+
+var contacts = {};
+
+cozysdk.queryView('contact', 'all', {include_docs: true}, function(err, entries){
+    if(err) return alert(err);
+    contacts = {}
+    entries.forEach(function(entry){
+        contacts[entry.id] = entry.doc
+    })
+    console.log(contacts)
+} );
